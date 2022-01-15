2c8fc09b9da0a0279cd60678918d39300f1fa7e8
Michel Betancourt <michelbetancourt23@gmail.com>
2022-01-14 22:03:53 -0400

    chore: update package.json

--- core/common/src/stream.ts
+++ core/common/src/stream.ts
@@ -1,4 +1,11 @@
-import { Duplex, Readable, Stream, Transform, Writable } from 'node:stream';
+import {
+  Duplex,
+  Readable,
+  Stream,
+  Transform,
+  TransformOptions,
+  Writable
+} from 'node:stream';
 import { hasProperty } from './type-guards';
 
 export function isStream(stream: unknown): stream is Stream {
@@ -49,4 +56,54 @@ export function isTransformStream(stream: unknown): stream is Transform {
     hasProperty(stream, '_transform') &&
     typeof stream._transform === 'function'
   );
+}
+
+export type Serializer = {
+  serialize: () => void;
+  deserialize: () => void;
+};
+
+export function through(transform: TransformOptions['transform']): Transform;
+
+export function through(
+  transform: TransformOptions['transform'],
+  flush: TransformOptions['flush'] | null
+): Transform;
+
+export function through(
+  options: TransformOptions,
+  transform: TransformOptions['transform'],
+  flush: TransformOptions['flush'] | null
+): Transform;
+
+export function through(
+  optionsOrTransform: TransformOptions | TransformOptions['transform'],
+  transformOrFlush?: TransformOptions['transform'] | TransformOptions['flush'] | null,
+  flush?: TransformOptions['flush'] | null
+) {
+  if (typeof optionsOrTransform === 'function') {
+    flush = transformOrFlush as never;
+    transformOrFlush = optionsOrTransform;
+    optionsOrTransform = {};
+  }
+
+  const t2 = new Transform({
+    objectMode: true,
+    highWaterMark: 16,
+    ...optionsOrTransform
+  });
+
+  if (typeof transformOrFlush !== 'function') {
+    // noop
+    transformOrFlush = (chunk, _, cb) => cb(null, chunk);
+  }
+
+  if (typeof flush !== 'function') {
+    flush = null;
+  }
+
+  t2._transform = transformOrFlush;
+  t2._flush = flush as never;
+
+  return t2;
 }
